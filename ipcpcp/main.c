b1c519d64e62021738fa8603a4a23fdcc1c95214
albert <leealbert@leematoMacBook-Air.local>
2015-04-14 22:57:54 +0800

    Initial Commit

---
+++ ipcpcp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ipcpcp
+//
+//  Created by albert on 15/4/14.
+//  Copyright (c) 2015年 albert. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
